b8b84061dfc90d07b218b49aea3c4fbe4c049304
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-07-07 21:18:54 +0100

    worked on components

---
+++ src/components/TabSelector.tsx
@@ -0,0 +1,47 @@
+import { Button, ButtonGroup } from "@suid/material";
+import { Accessor, For } from "solid-js";import Tooltip from "./Tooltip";
+
+
+interface TabItem {
+  label: string;
+  value: string;
+  name: string;
+}
+
+const tabItems: TabItem[] = [
+  { label: "Core planetary positions, transitions and special degrees", value: "core", name: "Core" },
+  { label: "Extended planetary positions over time", value: "extended", name: "Extended" },
+  { label: "Sun, planetary and transposed transitions", value: "transitions", name: "Transitions" },
+  { label: "Planetary motions and retrograde phases over time", value: "stations", name: "Motions" },
+];
+
+export default function TabSelector({ pane, setPane }: { pane: Accessor<string>, setPane: Function;   }) {
+  
+
+  const tabSelectorClasses = (): string => {
+    return ['tab-selector', pane()].join(' ');
+  }
+
+  const itemType = (key: string): "contained" | "outlined" => {
+    if (pane() === key) {
+      return 'contained';
+    } else {
+      return 'outlined';
+    }
+  }
+
+  const handleChange = (e: Event) => {
+    if (e.target instanceof HTMLButtonElement) { 
+      if (e.target.value) {
+        setPane(e.target.value);
+      }
+      
+    }
+  }
+
+  return <ButtonGroup class={tabSelectorClasses()} variant="outlined" aria-label="outlined button group" onClick={handleChange}>
+    <For each={tabItems}>
+      {(item) => <Tooltip label={item.label}><Button variant={itemType(item.value)}  value={item.value}>{item.name}</Button></Tooltip>}
+    </For>
+  </ButtonGroup>
+}
