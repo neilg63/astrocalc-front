dedda98ee3f014b604a46dddd1e2ff30f7dcd4fd
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-07-08 22:51:12 +0100

    worked on components

--- src/components/SlideToggle.tsx
+++ src/components/SlideToggle.tsx
@@ -3,12 +3,32 @@ import { Accessor } from "solid-js";
 import { notEmptyString } from "~/api/utils";
 
 export default function SlideToggle({ offName, onName, isOn, onChange, key }: { offName: string, onName: string; isOn: Accessor<boolean | undefined>; onChange: Function; key?: string }) {
-  const keyClass = notEmptyString(key) ? key : "value-toggle";
-  const statusClass = () => isOn() ? "on" : "off";
-  const wrapperClasses = ["field", "flex", "flex-row", keyClass, statusClass()].join(" ");
-  return <div class={wrapperClasses}>
-    <label class="off-label">{ offName }</label>
+  
+  const buildWrapperClasses = () => {
+    const cls = ["field", "flex", "flex-row", "value-toggle"];
+    if (notEmptyString(key)) {
+      cls.push(key as string);
+    }
+    if (isOn()) {
+      cls.push("on");
+    } else {
+      cls.push("off");
+    }
+    return cls.join(" ")
+  }
+  const setOn = () => {
+    if (!isOn()) {
+      onChange();
+    }
+  }
+  const setOff = () => {
+    if (isOn()) {
+      onChange();
+    }
+  }
+  return <div class={buildWrapperClasses()}>
+      <label class="off-label" onClick={() => setOff()}>{ offName }</label>
       <Switch checked={isOn()} onChange={() => onChange()} />
-      <label class="on-label">{onName}</label>
+      <label class="on-label" onClick={() => setOn()}>{onName}</label>
     </div>
 }
